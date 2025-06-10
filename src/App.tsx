6cbe87ef8c5c8ca0fef90ba8042f7b7949990630
Priyanshi333 <162355496+Priyanshi333@users.noreply.github.com>
2025-06-10 11:14:24 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import "./App.css";
+mport "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
 import Institute from "./pages/dashboards/instiute";
