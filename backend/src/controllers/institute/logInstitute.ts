c7a54f5948828a797f5b1540819bde7a8cc92b02
Brijesh Singh <coderrrx@gmail.com>
2025-06-21 14:26:47 +0530

    fixed naming

--- backend/src/controllers/institute/logInstitute.ts
+++ backend/src/controllers/institute/logInstitute.ts
@@ -1,6 +1,8 @@
+import  Institute  from "@/models/institute/insitituteModel"
 import { Request, Response } from "express";
-
+import { hashPassword, comparePassword } from "@/utils/institute/hashAuth";
 import {instituteLoginSchema} from "@/validations/instituteValidation";
+
 
 const logInInstitute = async (req: Request, res: Response): Promise<void> => {
     try{
@@ -11,6 +13,18 @@ const logInInstitute = async (req: Request, res: Response): Promise<void> => {
             return;
         }
         const { contactEmail, password } = parsed.data;
+        const foundEmail = await Institute.findOne({contactEmail})
+
+        if(!foundEmail) {res.status(400).json({error: "Email not found"}); return;}
+
+
+        const isPasswordValid = await comparePassword(password, foundEmail.password);
+        if (!isPasswordValid) {
+                res.status(400).json({ error: "Invalid password" });
+                return;
+            }
+        res.status(200).json({ message: "Login successful" });
+
     } catch (error) {
         res.status(500).json({ error: "Internal server error" });
     }
